283206b0a19af5e9b82d58455e2bb3bf6c4bc0d5
Sylvia <sylviapap1@gmail.com>
2020-04-12 18:42:15 -0700

    created basic controller actions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,25 @@
 class SubscriptionsController < ApplicationController
+    def new
+    end
+
+    def index
+        subscriptions = Subscription.all
+        render json: subscriptions
+    end
+
+    def create
+        subscription = Subscription.create(subscription_params)
+        render json: subscription
+    end
+
+    def show
+        subscription = Subscription.find(params[:id])
+        render json: subscription
+    end
+
+    private
+
+    def subscription_params
+        params.require(:subscription).permit(:company, :cost)
+    end
 end
--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -1,2 +1,33 @@
 class UserSubscriptionsController < ApplicationController
+    def index
+        user_subscriptions = UserSubscription.all
+        render json: user_subscriptions
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+        user = User.find(params[:user_id])
+        subscription = Subscription.find(params[:subscription_id])
+        start_date = (params[:start_date])
+        end_date = (params[:end_date])
+        
+        user_subscription = UserSubscription.new(user_subscription_params)
+        if user_subscription.save
+            render json: user_subscription
+        else
+            render json: {message: "UserSubscription error"}
+        end
+
+    end
+
+    private
+
+    def user_subscription_params
+        params.require(:user_subscription).permit(:user_id, :subscription_id, :start_date, :end_date)
+    end
 end
