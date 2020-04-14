ed604594ea789305e1e65fbe5f458526da604996
Sylvia <sylviapap1@gmail.com>
2020-04-13 22:54:18 -0700

    pw _digest

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::AuthController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        
+        if user && user.authenticate(params[:password])
+          my_token = issue_token(user)
+    
+          render json: {id: user.id, username: user.username, token: my_token}
+        else
+          render json: {error: 'That user could not be found'}, status: 401
+        end
+    end
+    
+    def show
+        if logged_in?
+            render json: { id: current_user.id, username: current_user.username }
+        else
+            render json: {error: 'No user could be found'}, status: 401
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :users
       resources :user_subscriptions
       resources :subscriptions
+      post '/auth', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
