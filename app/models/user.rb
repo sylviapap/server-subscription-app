985974e39c8a5e53d123cf60398b63e4f1bc8cb6
Sylvia <sylviapap1@gmail.com>
2020-04-13 22:22:00 -0700

    password setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :user_subscriptions, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_012022) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
