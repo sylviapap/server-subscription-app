939ddeab84668a5b63b339742b6745585f5de90d
Sylvia <sylviapap1@gmail.com>
2020-04-15 11:53:39 -0700

    has many through

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    
+
     validates :username, uniqueness: { case_sensitive: false }
     validates :username, presence: true
 
     has_many :user_subscriptions
+    has_many :subscriptions, through: :user_subscriptions
 end
