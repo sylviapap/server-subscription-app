cfa145dd17222f709b346d91aa6c3b6aef6ef971
Sylvia <sylviapap1@gmail.com>
2020-04-15 11:58:18 -0700

    has many through

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,4 +1,6 @@
 class SubscriptionSerializer < ActiveModel::Serializer
   has_many :user_subscriptions
+  has_many :users, through: :user_subscriptions
+
   attributes :id, :company, :cost
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :user_subscriptions
+  has_many :subscriptions, through: :user_subscriptions
   attributes :id, :username
 end
