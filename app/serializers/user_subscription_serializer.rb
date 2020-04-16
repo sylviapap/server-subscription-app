aed298eaf0e0f5f7b4eba9f0faffd6265d7801d2
Sylvia <sylviapap1@gmail.com>
2020-04-16 00:27:00 -0700

    added :subscription

--- app/serializers/user_subscription_serializer.rb
+++ app/serializers/user_subscription_serializer.rb
@@ -1,5 +1,5 @@
 class UserSubscriptionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :subscription_id, :start_date, :end_date, :next_payment
+  attributes :id, :user_id, :subscription_id, :start_date, :end_date, :next_payment, :subscription
 
   belongs_to :subscription
   belongs_to :user
